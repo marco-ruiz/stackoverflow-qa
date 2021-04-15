f76f90e3302d3af13f32be468979f1dfa5d33c7f
Marco Ruiz <marco.a.ruiz@gmail.com>
2021-04-15 00:14:52 -0400

    so-67102115

---
+++ so-67102115/src/test/java/so/MonoSwitchIfEmptyTest.java
@@ -0,0 +1,68 @@
+package so;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class MonoSwitchIfEmptyTest {
+
+	@Test
+	void whenRequestThrowsPrecompiledException_thenExpectDesiredStackTrace() {
+		trySwitchIfEmptyWithDesiredStackTrace(createAppropriateSwitchIfEmptyMono(true), true);
+	}
+
+	@Test
+	void whenRequestThrowsLiveException_thenExpectUndesiredStackTrace() {
+		trySwitchIfEmptyWithDesiredStackTrace(createAppropriateSwitchIfEmptyMono(false), false);
+	}
+
+	// This is done here to force the creation of an extra stack element into the stack trace.
+	// That stack element is the one that later will be checked to determine if the test passed or not.
+	private Mono<Void> createAppropriateSwitchIfEmptyMono(boolean desiredStackTrace) {
+		return desiredStackTrace ?
+				Mono.error(new LoggedRuntimeException()) :
+				Mono.defer(() -> Mono.error(new LoggedRuntimeException()));
+	}
+
+	private void trySwitchIfEmptyWithDesiredStackTrace(Mono<Void> switchIfEmptyMono, boolean desiredStackTrace) {
+		Mono<Void> loggedError = Mono.fromRunnable(() -> System.out.println("Throwing exception..."))
+				.then(switchIfEmptyMono);
+
+		Mono<Object> testedPipeline = Mono.fromRunnable(() -> loggedBusyWork(1000))
+				.switchIfEmpty(loggedError);
+
+		StepVerifier.create(testedPipeline)
+				.expectErrorMatches(e -> ((LoggedRuntimeException)e).isRelevantStackTrace() == desiredStackTrace)
+				.verify();
+	}
+
+	private void loggedBusyWork(int millis) {
+		long startTime = System.currentTimeMillis();
+		System.out.println("Starting busy work @ " + startTime + "...");
+		while (System.currentTimeMillis() - startTime < millis);
+		System.out.println("End busy work @ " + System.currentTimeMillis());
+	}
+
+	static class LoggedRuntimeException extends RuntimeException {
+
+		public LoggedRuntimeException() {
+			System.out.println("Creating exception...");
+			String stackTrace = getStackTraceStr();
+			System.out.println("Stack trace: \n" + stackTrace);
+		}
+
+		private String getStackTraceStr() {
+			StringWriter writer = new StringWriter();
+			printStackTrace(new PrintWriter(writer));
+			return writer.toString();
+		}
+
+		public boolean isRelevantStackTrace() {
+			return getStackTrace()[1].toString().contains(MonoSwitchIfEmptyTest.class.getName());
+		}
+	}
+}
